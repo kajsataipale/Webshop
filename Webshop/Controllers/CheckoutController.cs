084b41aba07933dc231b67e005f9e18214ca7652
“Kajsa <31646322+kajsataipale@users.noreply.github.com>
2018-03-28 15:06:44 +0200

    added the checkout page

---
+++ Webshop/Controllers/CheckoutController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Webshop.Models;
+//using System.Data.SqlClient;
+using Dapper;
+using MySql.Data.MySqlClient;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Webshop.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private readonly string connectionString;
+
+        public CheckoutController(IConfiguration configuration)
+        {
+            this.connectionString = configuration.GetConnectionString("ConnectionString");
+        }
+
+        public IActionResult Index()
+        {
+            //var cartId = Request.Cookies["CartID"];
+            List<CheckoutViewModel> cart;
+            using (var connection = new MySqlConnection(this.connectionString))
+            {
+                cart = connection.Query<CheckoutViewModel>("SELECT * FROM Cart INNER JOIN Products ON Cart.product_id=Products.product_id").ToList();
+            }
+
+            return View(cart);
+        }
+ 
+
+                
+
+
+
+
+
+
+    }
+}
---
+++ Webshop/Models/CheckoutViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Webshop.Models
+{
+    public class CheckoutViewModel
+    {
+        public int Id { get; set; }
+        public int product_id { get; set; }
+        public int price { get; set; }
+    
+
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+    }
+}
