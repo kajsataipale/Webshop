57dcb43c6f9952c1d628abf2f4174de11986e7dc
“Kajsa <31646322+kajsataipale@users.noreply.github.com>
2018-04-05 12:41:18 +0200

    started with the unittest

---
+++ Webshop.Unittest/ProductServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit;
+using NUnit.Framework;
+using Webshop.Project.Core.Repositories;
+using Webshop.Project.Core.Servies.Implementations;
+using FakeItEasy;
+using Webshop.Project.Core.Models;
+using Webshop.Project.Core.Repositories.Implementations;
+
+namespace Webshop.Unittest
+{
+    public class ProductServiceTests
+    {
+        private ProductService productService;
+        private ProductRepository productRepository;
+    }
+}
